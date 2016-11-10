bd6449a614fa93a5d4e0f573ec3b913d2417cce0
Laura Moreno <stereofonik.imgs@gmail.com>
2016-11-10 14:54:33 -0600

    Set-up basic skeleton, seeing hello world at index

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# from models import Text
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
---
+++ helpers.py
@@ -0,0 +1,6 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.externals import joblib
+
