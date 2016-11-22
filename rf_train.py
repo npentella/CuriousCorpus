6e2b09d478d61c2ae66959b87cb58689b57436b0
Rachael Lunney <email": null>
2016-11-22 14:39:06 -0600

    Create Random Forest vectorizer and classifier pickles

---
+++ rf_train.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+
+print "Grabbing data..."
+
+training_text_collection_f = open("training_text_collection.pkl", "rb")
+training_text_collection = joblib.load(training_text_collection_f)
+training_text_collection_f.close()
+
+training_targets_f = open("training_targets.pkl", "rb")
+training_targets = joblib.load(training_targets_f)
+training_targets_f.close()
+
+print("Vectorizing data...")
+
+vectorizer = TfidfVectorizer(analyzer = "word",   \
+                             tokenizer = None,    \
+                             preprocessor = None, \
+                             stop_words = "english",   \
+                             max_features = 2500,
+                             min_df = 5,
+                             max_df = 0.25)
+
+
+train_tfidf = vectorizer.fit_transform(training_text_collection)
+
+save_vect = open("rf_tfidf_vect.pkl", "wb")
+joblib.dump(vectorizer, save_vect)
+save_vect.close()
+
+clf = RandomForestClassifier(n_estimators = 250).fit(train_tfidf, training_targets)
+
+save_clf = open("rf_clf.pkl", "wb")
+joblib.dump(clf, save_clf)
+save_clf.close()
